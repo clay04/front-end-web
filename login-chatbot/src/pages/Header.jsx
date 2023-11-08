f8f33a49966da1f54be221247c67443d1d160935
Clay04 <s22110530@student.unklab.ac.id>
2023-11-08 21:08:30 +0800

    master

---
+++ login-chatbot/src/pages/Header.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div class="p-5 bg-primary text-white text-center rounded">
+                <h1>Welcome to you, Angeli !!!</h1>
+                <p>A chatbot is a software or computer program that simulates
+                human conversation through text or voice interactions.</p>
+                <button class="btn btn-info btn-sm">Read More !!!</button>
+            </div>
+        );
+    }
+}
+
+
+export default Header;
--- test-router/src/App.js
+++ test-router/src/App.js
@@ -1,24 +1,23 @@
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Blogs from "./pages/Blogs";
-import Contact from "./pages/Contact";
-import NoPage from "./pages/NoPages";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout.js";
+import Home from "./pages/Home.js";
+import Blogs from "./pages/Blogs.js";
+import Contact from "./pages/Contact.js";
+import NoPage from "./pages/NoPages.js";
 
 function App() {
-  return (
+ return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home/>} />
-          <Route path="blogs" element={<Blogs/>} />
-          <Route path="contact" element={<Contact/>} />
-          <Route path="*" element={<NoPage/>} />
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contacts" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+ );
 }
-
 
 export default App;
